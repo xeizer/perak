dfc8c38925073fabb6e6b5b72460970b1394d74c
xeizer <keiser.form@gmail.com>
2023-03-01 11:16:25 +0700

    Membuat Form

--- resources/views/livewire/laporan.blade.php
+++ resources/views/livewire/laporan.blade.php
@@ -7,6 +7,20 @@
 
                 <div class="card-body">
                     <h5>Halaman Pelaporan Masyarakat</h5>
+                    <label>Tanggal Pengaduan</label>
+                    <input type="date" class="form-control" />
+
+                    <label>NIK</label>
+                    <input type="text" class="form-control" />
+
+                    <label>Isi Laporan</label>
+                    <textarea class="form-control"></textarea>
+
+                    <label>Foto</label>
+                    <input type="file" class="form-control" />
+
+                    <button class="btn btn-outline-success w-100 mt-4">SIMPAN</button>
+
 
                 </div>
             </div>
